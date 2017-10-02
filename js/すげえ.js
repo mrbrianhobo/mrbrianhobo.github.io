ff637fa20280352a5c95287052a061ca88fb9c02
Brian Ho <mr.brianhobo@hotmail.com>
2017-10-02 03:49:20 -0700

    update website

--- js/すげえ.js
+++ js/すげえ.js
@@ -1,9 +1,20 @@
 $(document).ready(function() {
+  $('.chaffle').chaffle();
   $('#particles').particleground({
     dotColor: '#9C8DF7',
     lineColor: '#9C8DF7'
   });
   $('.intro').css({
     'margin-top': -($('.intro').height() / 2)
+  });
+  $('#typed').typed({
+    strings: ['problem solving.', 'blockchain.', 'crypto.', 'building things.', 'breaking things.', 'anime.'],
+    showCursor: true,
+    typeSpeed: 40,
+    backSpeed: 20,
+    shuffle: true,
+    startDelay: 1000,
+    backDelay: 1000, 
+    loop: true,  
   });
 });
